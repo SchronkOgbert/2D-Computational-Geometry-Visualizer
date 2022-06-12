095e8cf56806c767d5934b7d062337e360317cef
Damian Sirbu <sirbudamian@gmail.com>
2022-06-12 15:17:20 +0300

    linked boost

--- GCProjectCode/GCProjectCode/GCProjectCode.cpp
+++ GCProjectCode/GCProjectCode/GCProjectCode.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/numeric/ublas/vector.hpp>
 
 int main()
 {
     boost::asio::io_context io;
+    boost::numeric::ublas::vector<int> v(3);
+    v[0] = 9;
+    v.resize(10);
+    std::cout << v[0] << '\n';
+    std::cout << v.size() << '\n';
+    v.insert_element(3, 23);
+    std::cout << v.size() << '\n';
 
     auto timer = boost::asio::steady_timer(io,
         boost::asio::chrono::seconds(1));
